ae682f8ff48ca1141ea8d3171598bc946ad5b993
Jaeyeon <Jaeyeon@DESKTOP-NQG8CF9>
2016-10-12 15:31:41 +0900

    10/12 15:31 object JAEYEON

---
+++ Koroupware/src/kosta/dept/model/Dept.java
@@ -0,0 +1,42 @@
+package kosta.dept.model;
+
+public class Dept {
+	private int dept_no;
+	private String dept_name;
+	private String dept_work;
+	private String dept_address;
+	private String dept_telephone;
+
+	public int getDept_no() {
+		return dept_no;
+	}
+	public void setDept_no(int dept_no) {
+		this.dept_no = dept_no;
+	}
+	public String getDept_name() {
+		return dept_name;
+	}
+	public void setDept_name(String dept_name) {
+		this.dept_name = dept_name;
+	}
+	public String getDept_work() {
+		return dept_work;
+	}
+	public void setDept_work(String dept_work) {
+		this.dept_work = dept_work;
+	}
+	public String getDept_address() {
+		return dept_address;
+	}
+	public void setDept_address(String dept_address) {
+		this.dept_address = dept_address;
+	}
+	public String getDept_telephone() {
+		return dept_telephone;
+	}
+	public void setDept_telephone(String dept_telephone) {
+		this.dept_telephone = dept_telephone;
+	}
+	
+	
+}
---
+++ Koroupware/src/kosta/emp/model/Office.java
@@ -0,0 +1,19 @@
+package kosta.emp.model;
+
+public class Office {
+	private int office_no;
+	private String office_name;
+	
+	public int getOffice_no() {
+		return office_no;
+	}
+	public void setOffice_no(int office_no) {
+		this.office_no = office_no;
+	}
+	public String getOffice_name() {
+		return office_name;
+	}
+	public void setOffice_name(String office_name) {
+		this.office_name = office_name;
+	}
+}
