d306d87eb814fa66d458b74120c282eb755fa762
sdc337dc <sdc337dc@DESKTOP-F79GR1G>
2016-10-13 10:55:41 +0900

    .

--- Koroupware/src/kosta/emp/model/Office.java
+++ Koroupware/src/kosta/emp/model/Office.java
@@ -5,7 +5,7 @@ public class Office {
 	private String office_name;
 	
 	public int getOffice_no() {
-		return office_no;
+		return office_no; 
 	}
 	public void setOffice_no(int office_no) {
 		this.office_no = office_no;
