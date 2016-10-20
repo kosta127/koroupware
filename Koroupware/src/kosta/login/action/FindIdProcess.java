fea8b42ca6f8cfd0ae2b4cb93075b91f3668ca30
sdc337dc <sdc337dc@DESKTOP-F79GR1G>
2016-10-20 14:59:07 +0900

    ff

--- Koroupware/src/kosta/login/action/FindIdProcess.java
+++ Koroupware/src/kosta/login/action/FindIdProcess.java
@@ -23,7 +23,7 @@ public class FindIdProcess implements Action{
 		String RESIDENTNUMBER = request.getParameter("find_RESIDENTNUMBER");
 		System.out.println(RESIDENTNUMBER);
 		HttpSession session = request.getSession();
-		  
+		   
 		for(int i=0; i<list.size(); i++){
 			if(list.get(i).getEmp_residentnumber().equals(RESIDENTNUMBER)){
 				System.out.println(list.get(i).getEmp_id());
